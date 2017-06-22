fe222e1e0c26c8194e449cf340afdaf3e794f81a
Jiming Bai <jmbai@safs.com>
2017-06-22 15:09:48 +0800

    gx

--- components/menu/menu.component.js
+++ components/menu/menu.component.js
@@ -1,7 +1,7 @@
 angular
   .module('menu')
   .component('menu',{
-    templateUrl: "components/menu/menu.template.html",
+    templateUrl: "../../components/menu/menu.template.html",
     controller:['$scope',function ($scope) {
 
     }],
